c2e4eca7fd3c544f625313ccdad9a7a4ce7a2b29
QK391 <90550481+QK391@users.noreply.github.com>
2023-10-06 11:20:38 +0700

    make department can be click #4

--- public/dashboard.js
+++ public/dashboard.js
@@ -11,13 +11,44 @@ async function getAllDepartments(url){
 function show(data){
     var ul = document.createElement('ul');
     ul.className="list-inline";
-    for(i=0;i<data.length;i++)
+    for(i=2; i < data.length; i++)
     {
         var li = document.createElement('li');
         li.className="list-inline-item";
-        li.innerHTML = data[i].ename;
+        li.innerHTML = data[0].ename;
         ul.appendChild(li);
+        li.style.cursor = 'pointer';
+        li.addEventListener('click',myFunction);
+        function myFunction(){
+            document.getElementById("man").innerHTML = "Hello Man";
+        } 
+        console.log(li.innerHTML)
+    }
+
+    for(i=2; i<data.length; i++)
+    {
+        var li = document.createElement('li');
+        li.className="list-inline-item";
+        li.innerHTML = data[1].ename;
+        ul.appendChild(li);
+        li.style.cursor = 'pointer';
+        li.addEventListener('click',myFunction);
+        function myFunction(){
+            document.getElementById("woman").innerHTML = "Hello Woman";
+        }  
+    }
     
+    for(i=2; i<data.length; i++)
+    {
+        var li = document.createElement('li');
+        li.className="list-inline-item";
+        li.innerHTML = data[2].ename;
+        ul.appendChild(li);
+        li.style.cursor = 'pointer';
+        li.addEventListener('click',myFunction);
+        function myFunction(){
+            document.getElementById("accessory").innerHTML = "Accessory";
+        }
     }
     document.getElementById('nav-link-department').appendChild(ul);
 }
